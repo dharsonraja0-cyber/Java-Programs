01b7f6b37dfa9fde28568afc1207d18fe87aca1c
Dharson <dharsonraja0@gmail.com>
2026-05-01 13:59:25 +0530

    constructors
    Signed-off-by: Dharson <dharsonraja0@gmail.com>

---
+++ sample8.java
@@ -0,0 +1,26 @@
+class sample7 {
+    public static void main(String[] args) {
+        
+        constructors o =new constructors("Dhanaraj");
+        constructors o2 =new constructors(o);
+        constructors o3 =new constructors();
+
+        System.out.println(o.name);
+        System.out.println(o2.name);
+
+    }
+}
+class constructors{
+    String name;
+    String name2;
+    constructors(){
+       System.out.println("hiiii");
+    }
+    constructors (String nm) {
+        name = nm;
+     
+    }
+    constructors (constructors o){
+        name2 = o.name;
+    }
+}
